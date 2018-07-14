6a96f6a386e9ce56634d60b066be24ebb0721cee
lesliesuhm <lesliesuhmiii@gmail.com>
2018-07-14 03:20:22 -0400

    Update VNC-AAI-16-04-XFCE4.sh

--- VNC-AAI-16-04-XFCE4.sh
+++ VNC-AAI-16-04-XFCE4.sh
@@ -1,29 +1,31 @@
-# VNC Server Installation Script for Ubuntu 16.04
+#!/bin/sh
+# VNC Server Installation Script for Ubuntu 14.04, and every version up, including 18.04.
 # Xfce4 Desktop Environment
-# Run with sudo and ./
-# You may need to chmod +x the file 
+# Run with sudo ./
+# You may need to chmod +x the file
 # Commands meant for current user will be run as current user NOT sudo
-# 
+#
 # Please Change VNCUSER Variable to Current User Before Running!
 # You will have to enter a password for VNC right after the the packages are installed
 #
-
-
+# This is the second version of this script, see CHANGELOG for all of the changes I have made.
+ 
+ 
 VNCUSER="change_to_current_user_before_running"
-
-
+ 
+ 
 #----------DO NOT EDIT BELOW THIS LINE---------------
-
+ 
 echo "Make sure to change the VNCUSER variable first!"
 sleep 3
-echo "You will need to enter a VNC password right after the package installation"
+echo "You will need to enter a VNC password after the package installation"
 sleep 5
 echo "Updating System Repostitories"
-apt -y update
+apt-get -y update
 sleep 1
 echo "Starting VNC Installation"
 sleep 1
-apt -y install xfce4 xfce4-goodies tightvncserver
+apt-get -y install xfce4 xfce4-goodies tightvncserver
 sleep 1
 echo "Packages installed, starting VNC configurations"
 sleep 1
@@ -33,11 +35,9 @@ sudo -u $VNCUSER vncserver -kill :1
 sleep 1
 sudo -u $VNCUSER mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 sleep 1
-sudo -u $VNCUSER wget https://raw.githubusercontent.com/lesliesuhm/VNC-Server-AAI/master/configs/xfce4_configs
+sudo -u $VNCUSER mv configs/xfce4_configs ~/.vnc/xstartup
 sleep 1
-sudo -u $VNCUSER mv xfce4_configs ~/.vnc/xstartup
-sleep 1
-chmod +x ~/.vnc/xstartup
+sudo -u $VNCUSER chmod +x ~/.vnc/xstartup
 sleep 1
 sudo -u $VNCUSER vncserver
 sleep 1
@@ -47,14 +47,12 @@ echo "VNC Server Successfully Installed!"
 echo "Start VNC Server with the following command"
 sleep 1
 echo " "
-echo "          vncserver      "
+echo "       vncserver      "
 echo " "
 sleep 1
 echo " "
 sleep 1
-echo " "
-sleep 1
-echo "All Credit goes to Leslie Suhm!" 
+echo "All Credit goes to Leslie Suhm!"
 sleep 1
 echo "Check us out at Github!"
 sleep 1
